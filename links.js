49fb4740b711febe7ccde98bb62cfc1bb3ae51ef
unknown <bediszitoun1919@gmail.com>
2020-10-19 22:59:36 +0100

    project

---
+++ links.js
@@ -0,0 +1,3 @@
+$('#c1').click(function() {
+    window.location.href = 'https://www.amazon.com/Hush-Hush-Stone-Barrington-Novel-Book-ebook/dp/B086J8N9RK';
+})
--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+$(".container button").css("font-size", "30px");
+$(".container button").css("color", "black");
+$(".container button").css("text-align", "center");
+$(".container button").css("width", "50%");
+$(".container button").css("background-color", "#d2b48c");
+$(".column").css("width", "33.33%");
+$(".column").css("padding", "100px");
+$("#topbutton").css("position", "fixed");
+$("#topbutton").css("bottom", "50");
+$("h3.description").css("border-style", "double");
+$("h3.description").css("border-color", "black");
+$("h3.read").css("border-style", "dotted");
+$("h3.read").css("border-color", "black");
+$(".read").css("color", "black");
+$(".container button").css("float", "center")
+
+
+
+
+
+
+
+
+
+
+
 $("#add").on("click", function(e) {
     e.preventDefault();
     var itemVal = $("#item").val();
